0d2b75970a6f95b365bbcd2bd57e1da157807bca
Cheesecake <octavioram5991@hotmail.com>
2017-06-07 17:27:40 -0600

    Paginación infinita
    Vamos a implementar una paginación de estilo “infinito”. Lo que queremos decir con esto es que primero mostramos, por ejemplo, 10 posts, con un enlace de “cargar más” en la parte inferior. Al hacer clic en este enlace se cargarán 10 más, y así hasta el infinito y más allá. Esto significa que podemos controlar todo nuestro sistema de paginación con un solo parámetro que representa el número de posts que mostraremos en pantalla.

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -1,40 +1,51 @@
 Router.configure({
-    
-    layoutTemplate: 'layout',
-    loadingTemplate: 'loading',
-    notFoundTemplate: 'notFound',
-    waitOn: function() {    
-     return [Meteor.subscribe('posts'), Meteor.subscribe('notifications')]
-    }
+  layoutTemplate: 'layout',
+  loadingTemplate: 'loading',
+  notFoundTemplate: 'notFound',
+  waitOn: function() { 
+    return [Meteor.subscribe('notifications')]
+  }
 });
 
-Router.route('/', {name: 'postsList'});
-
 Router.route('/posts/:_id', {
-    name: 'postPage',
-    waitOn: function() {    
-        return Meteor.subscribe('comments', this.params._id);  
-    },
-    data: function() { return Posts.findOne(this.params._id); }
+  name: 'postPage',
+  waitOn: function() {
+    return Meteor.subscribe('comments', this.params._id);
+  },
+  data: function() { return Posts.findOne(this.params._id); }
 });
 
 Router.route('/posts/:_id/edit', {
-    name: 'postEdit',
-    data: function() { return Posts.findOne(this.params._id); }
+  name: 'postEdit',
+  data: function() { return Posts.findOne(this.params._id); }
 });
 
 Router.route('/submit', {name: 'postSubmit'});
 
+Router.route('/:postsLimit?', {
+  name: 'postsList',
+  waitOn: function() {
+    var limit = parseInt(this.params.postsLimit) || 5; 
+    return Meteor.subscribe('posts', {sort: {submitted: -1}, limit: limit});
+  },
+  data: function() {
+    var limit = parseInt(this.params.postsLimit) || 5; 
+    return {
+      posts: Posts.find({}, {sort: {submitted: -1}, limit: limit})
+    };
+  }
+});
+
 var requireLogin = function() {
-    if (! Meteor.user()) {
-        if (Meteor.loggingIn()) {
-            this.render(this.loadingTemplate);
-        } else {
-            this.render('accessDenied');
-        }
+  if (! Meteor.user()) {
+    if (Meteor.loggingIn()) {
+      this.render(this.loadingTemplate);
     } else {
-        this.next();
+      this.render('accessDenied');
     }
+  } else {
+    this.next();
+  }
 }
 
 Router.onBeforeAction('dataNotFound', {only: 'postPage'});
